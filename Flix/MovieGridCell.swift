cc94f1a3deabaa2be92298c53458374a60b81802
Josh Harris <81477294+JHarrisJoshua@users.noreply.github.com>
2021-09-30 18:48:02 -0400

    project 2 - crash

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Joshua Harris on 9/30/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+}
